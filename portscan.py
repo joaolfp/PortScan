5bc41c97e404771358e262144007b839a83e7498
João Lucas <joaolucasfp2001@gmail.com>
2021-12-12 01:07:33 -0300

    Updated header in port scan

--- portscan.py
+++ portscan.py
@@ -1,5 +1,31 @@
 import socket
 
+def header():
+    print("""
+ ########:::'#######::'########::'########:::::'######:::'######:::::'###::::'##::: ##:
+ ##.... ##:'##.... ##: ##.... ##:... ##..:::::'##... ##:'##... ##:::'## ##::: ###:: ##:
+ ##:::: ##: ##:::: ##: ##:::: ##:::: ##::::::: ##:::..:: ##:::..:::'##:. ##:: ####: ##:
+ ########:: ##:::: ##: ########::::: ##:::::::. ######:: ##:::::::'##:::. ##: ## ## ##:
+ ##.....::: ##:::: ##: ##.. ##:::::: ##::::::::..... ##: ##::::::: #########: ##. ####:
+ ##:::::::: ##:::: ##: ##::. ##::::: ##:::::::'##::: ##: ##::: ##: ##.... ##: ##:. ###:
+ ##::::::::. #######:: ##:::. ##:::: ##:::::::. ######::. ######:: ##:::: ##: ##::. ##:
+..::::::::::.......:::..:::::..:::::..:::::::::......::::......:::..:::::..::..::::..::
+""")
+    print("")
+    print("About: To check open ports on a particular host")
+    print("Author: Joao Lucas")
+    print("Language: Python")
+    print("GitHub: https://github.com/joaolfp/PortScan")
+    print("")
+
+def menu(value):
+    if value == "1":
+       addFields()
+    elif value == "2":
+       quantityPorts()
+    else:
+       print("Option not found")   
+
 def setupClient(host, port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.settimeout(0.5)
@@ -24,16 +50,11 @@ def quantityPorts():
     for port in ports:
         setupClient(host, port)
         
-def menu(value):
-    if value == "1":
-       addFields()
-    elif value == "2":
-       quantityPorts()
-    else:
-       print("Option not found")
-
-print("1 - Add by fields")
-print("2 - Add by quantity ports")    
-
-value = input("Choose an option: ")
-menu(value)
+if __name__ == "__main__":
+    header()
+    print("------------------------------")
+    print("1 - Add by fields")
+    print("2 - Add by quantity ports") 
+    value = input("Choose an option: ")
+    print("")
+    menu(value)
